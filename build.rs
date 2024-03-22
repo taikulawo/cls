516d038245d29af790bd770fa2c6d5f735f81961
taikulawo <qaq1362211689@gmail.com>
2024-03-22 23:28:56 +0800

    tar follow symbol link

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     // 将 data 打包成tar.gzip
     Command::new("tar")
-        .args(["zcvf", "data.tar.gz", "data"])
+        .args(["zcvfh", "data.tar.gz", "data"])
         .spawn()
         .unwrap();
 }
